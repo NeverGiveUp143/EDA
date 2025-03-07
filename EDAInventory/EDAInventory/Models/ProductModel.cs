00862d1de4d2f6ac4236b1bc3b9c9a357eb65831
Venkatesh Kandregula <K.venkatesh007.143@gmail.com>
2025-03-07 15:39:13 +0530

    added model properties

--- EDAInventory/EDAInventory/Models/ProductModel.cs
+++ EDAInventory/EDAInventory/Models/ProductModel.cs
@@ -1,6 +1,11 @@
-﻿namespace EDAInventory.Models
+﻿#nullable disable
+namespace EDAInventory.Models
 {
     public class ProductModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public Guid ProductId { get; set; }
     }
 }
