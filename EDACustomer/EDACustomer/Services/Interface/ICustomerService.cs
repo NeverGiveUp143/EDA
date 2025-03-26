6a7bd9b056104de2155cb3c1a67ba51c2f8a1ee0
Venkatesh Kandregula <K.venkatesh007.143@gmail.com>
2025-03-26 16:14:20 +0530

    Exchange Type changes

--- EDACustomer/EDACustomer/Services/Interface/ICustomerService.cs
+++ EDACustomer/EDACustomer/Services/Interface/ICustomerService.cs
@@ -5,6 +5,6 @@ namespace EDACustomer.Services.Interface
     public interface ICustomerService
     {
         Task UpdateStockAndNotify(Guid productId, int newStock);
-        Task<string> UpdatedProduct(Guid productId, int checkedOutQuantity);
+        Task<ProductModel> UpdatedProduct(Guid productId, int checkedOutQuantity);
     }
 }
