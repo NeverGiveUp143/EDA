a161b9030671cc8d705babf81b23170cc9bd5236
Venkatesh Kandregula <venkatesh.kandregula@cognine.com>
2025-04-17 03:25:57 +0530

    push

--- EmailService/EmailService/Models/EventMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿#nullable disable
-namespace EmailService.Models
-{
-    public class EventMessage<T>
-    {
-        public string EventType { get; set; }
-        public T Data { get; set; }
-    }
-}
